7e935496b64d7b433efdecfb4e4b25b497b7943b
RSVlad <radlovackivladimir@gmail.com>
2021-07-10 19:45:16 +0200

    Version 1.1.0 is available!
    -Added "Fallout76Prefs.ini" finder button.
    -Made it possible to automate the ini file generation system!

---
+++ Better_Fallout76/Form1.Designer.cs
@@ -0,0 +1,393 @@
+﻿
+namespace Better_Fallout76
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.generateButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.checkBox18 = new System.Windows.Forms.CheckBox();
+            this.checkBox17 = new System.Windows.Forms.CheckBox();
+            this.checkBox16 = new System.Windows.Forms.CheckBox();
+            this.checkBox15 = new System.Windows.Forms.CheckBox();
+            this.checkBox14 = new System.Windows.Forms.CheckBox();
+            this.checkBox13 = new System.Windows.Forms.CheckBox();
+            this.checkBox12 = new System.Windows.Forms.CheckBox();
+            this.checkBox11 = new System.Windows.Forms.CheckBox();
+            this.checkBox10 = new System.Windows.Forms.CheckBox();
+            this.checkBox9 = new System.Windows.Forms.CheckBox();
+            this.checkBox8 = new System.Windows.Forms.CheckBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // generateButton
+            // 
+            this.generateButton.Location = new System.Drawing.Point(18, 369);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(105, 50);
+            this.generateButton.TabIndex = 7;
+            this.generateButton.Text = "Generate";
+            this.generateButton.UseVisualStyleBackColor = true;
+            this.generateButton.Click += new System.EventHandler(this.GenerateButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox4);
+            this.groupBox1.Controls.Add(this.checkBox3);
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(689, 148);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Fallout76Custom.ini configs";
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(6, 112);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(616, 23);
+            this.checkBox4.TabIndex = 15;
+            this.checkBox4.Text = "Disable Fog (Note: This might cause weird effects inside of certain areas such as" +
+    " forests)";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(6, 83);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(105, 23);
+            this.checkBox3.TabIndex = 14;
+            this.checkBox3.Text = "Disable Rain";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(6, 54);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(148, 23);
+            this.checkBox2.TabIndex = 13;
+            this.checkBox2.Text = "Disable Radial Blur";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(157, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 19);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Resolution Height:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(287, 53);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(78, 26);
+            this.textBox2.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(157, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 19);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Resolution Width:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(287, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(78, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(6, 25);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(113, 23);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Disable Blood";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.checkBox18);
+            this.groupBox2.Controls.Add(this.checkBox17);
+            this.groupBox2.Controls.Add(this.checkBox16);
+            this.groupBox2.Controls.Add(this.checkBox15);
+            this.groupBox2.Controls.Add(this.checkBox14);
+            this.groupBox2.Controls.Add(this.checkBox13);
+            this.groupBox2.Controls.Add(this.checkBox12);
+            this.groupBox2.Controls.Add(this.checkBox11);
+            this.groupBox2.Controls.Add(this.checkBox10);
+            this.groupBox2.Controls.Add(this.checkBox9);
+            this.groupBox2.Controls.Add(this.checkBox8);
+            this.groupBox2.Controls.Add(this.checkBox7);
+            this.groupBox2.Controls.Add(this.checkBox6);
+            this.groupBox2.Controls.Add(this.checkBox5);
+            this.groupBox2.Location = new System.Drawing.Point(12, 166);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(689, 185);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Fallout76Prefs.ini configs";
+            // 
+            // checkBox18
+            // 
+            this.checkBox18.AutoSize = true;
+            this.checkBox18.Location = new System.Drawing.Point(185, 141);
+            this.checkBox18.Name = "checkBox18";
+            this.checkBox18.Size = new System.Drawing.Size(152, 23);
+            this.checkBox18.TabIndex = 29;
+            this.checkBox18.Text = "Better PipBoy Menu";
+            this.checkBox18.UseVisualStyleBackColor = true;
+            // 
+            // checkBox17
+            // 
+            this.checkBox17.AutoSize = true;
+            this.checkBox17.Location = new System.Drawing.Point(6, 141);
+            this.checkBox17.Name = "checkBox17";
+            this.checkBox17.Size = new System.Drawing.Size(134, 23);
+            this.checkBox17.TabIndex = 28;
+            this.checkBox17.Text = "Disable Gamepad";
+            this.checkBox17.UseVisualStyleBackColor = true;
+            // 
+            // checkBox16
+            // 
+            this.checkBox16.AutoSize = true;
+            this.checkBox16.Location = new System.Drawing.Point(404, 112);
+            this.checkBox16.Name = "checkBox16";
+            this.checkBox16.Size = new System.Drawing.Size(162, 23);
+            this.checkBox16.TabIndex = 27;
+            this.checkBox16.Text = "Disable Water Depth";
+            this.checkBox16.UseVisualStyleBackColor = true;
+            // 
+            // checkBox15
+            // 
+            this.checkBox15.AutoSize = true;
+            this.checkBox15.Location = new System.Drawing.Point(185, 112);
+            this.checkBox15.Name = "checkBox15";
+            this.checkBox15.Size = new System.Drawing.Size(195, 23);
+            this.checkBox15.TabIndex = 26;
+            this.checkBox15.Text = "Disable Water Reflections";
+            this.checkBox15.UseVisualStyleBackColor = true;
+            // 
+            // checkBox14
+            // 
+            this.checkBox14.AutoSize = true;
+            this.checkBox14.Location = new System.Drawing.Point(6, 112);
+            this.checkBox14.Name = "checkBox14";
+            this.checkBox14.Size = new System.Drawing.Size(116, 23);
+            this.checkBox14.TabIndex = 25;
+            this.checkBox14.Text = "Disable Bokeh";
+            this.checkBox14.UseVisualStyleBackColor = true;
+            // 
+            // checkBox13
+            // 
+            this.checkBox13.AutoSize = true;
+            this.checkBox13.Location = new System.Drawing.Point(406, 83);
+            this.checkBox13.Name = "checkBox13";
+            this.checkBox13.Size = new System.Drawing.Size(144, 23);
+            this.checkBox13.TabIndex = 24;
+            this.checkBox13.Text = "Disable Lens Flare";
+            this.checkBox13.UseVisualStyleBackColor = true;
+            // 
+            // checkBox12
+            // 
+            this.checkBox12.AutoSize = true;
+            this.checkBox12.Location = new System.Drawing.Point(185, 83);
+            this.checkBox12.Name = "checkBox12";
+            this.checkBox12.Size = new System.Drawing.Size(118, 23);
+            this.checkBox12.TabIndex = 23;
+            this.checkBox12.Text = "Disable VSync";
+            this.checkBox12.UseVisualStyleBackColor = true;
+            // 
+            // checkBox11
+            // 
+            this.checkBox11.AutoSize = true;
+            this.checkBox11.Location = new System.Drawing.Point(6, 83);
+            this.checkBox11.Name = "checkBox11";
+            this.checkBox11.Size = new System.Drawing.Size(161, 23);
+            this.checkBox11.TabIndex = 22;
+            this.checkBox11.Text = "Disable HiRez Water";
+            this.checkBox11.UseVisualStyleBackColor = true;
+            // 
+            // checkBox10
+            // 
+            this.checkBox10.AutoSize = true;
+            this.checkBox10.Location = new System.Drawing.Point(406, 54);
+            this.checkBox10.Name = "checkBox10";
+            this.checkBox10.Size = new System.Drawing.Size(149, 23);
+            this.checkBox10.TabIndex = 21;
+            this.checkBox10.Text = "Disable Reflections";
+            this.checkBox10.UseVisualStyleBackColor = true;
+            // 
+            // checkBox9
+            // 
+            this.checkBox9.AutoSize = true;
+            this.checkBox9.Location = new System.Drawing.Point(185, 54);
+            this.checkBox9.Name = "checkBox9";
+            this.checkBox9.Size = new System.Drawing.Size(152, 23);
+            this.checkBox9.TabIndex = 20;
+            this.checkBox9.Text = "Disable Motion Blur";
+            this.checkBox9.UseVisualStyleBackColor = true;
+            // 
+            // checkBox8
+            // 
+            this.checkBox8.AutoSize = true;
+            this.checkBox8.Location = new System.Drawing.Point(6, 54);
+            this.checkBox8.Name = "checkBox8";
+            this.checkBox8.Size = new System.Drawing.Size(173, 23);
+            this.checkBox8.TabIndex = 19;
+            this.checkBox8.Text = "Disable Depth of Field";
+            this.checkBox8.UseVisualStyleBackColor = true;
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.Location = new System.Drawing.Point(406, 25);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(196, 23);
+            this.checkBox7.TabIndex = 18;
+            this.checkBox7.Text = "Disable Ambient Occlusion";
+            this.checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(185, 25);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(202, 23);
+            this.checkBox6.TabIndex = 17;
+            this.checkBox6.Text = "Disable Volumetric Lighting";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(6, 25);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(146, 23);
+            this.checkBox5.TabIndex = 16;
+            this.checkBox5.Text = "Disable Anisotropy";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(157, 385);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(556, 19);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Credits to Bialgo and his Fallout 76 Tweaker (I got a lot of information from the" +
+    "re)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(725, 443);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.generateButton);
+            this.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button generateButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.CheckBox checkBox8;
+        private System.Windows.Forms.CheckBox checkBox9;
+        private System.Windows.Forms.CheckBox checkBox10;
+        private System.Windows.Forms.CheckBox checkBox11;
+        private System.Windows.Forms.CheckBox checkBox12;
+        private System.Windows.Forms.CheckBox checkBox13;
+        private System.Windows.Forms.CheckBox checkBox14;
+        private System.Windows.Forms.CheckBox checkBox15;
+        private System.Windows.Forms.CheckBox checkBox16;
+        private System.Windows.Forms.CheckBox checkBox17;
+        private System.Windows.Forms.CheckBox checkBox18;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Better_Fallout76/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Windows.Forms;
+using AutoUpdaterDotNET;
+
+namespace Better_Fallout76
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        /* Intro */
+        readonly string ini0 = ("# This INI File was made with Better Fallout76 by RSVlad\n# The INI File has been coppied to your clipboard!\n\n");
+        /* Blood */ string ini1;
+        /* Resolution Width */ string ini2;
+        /* Resolution Height */ string ini3;
+        /* Disable Radial Blur */ string ini4;
+        /* Disable Rain */ string ini5;
+        /* Disable Fog */ string ini6;
+        /* Disable Anisotropy */ string ini7;
+        /* Disable Volumetric Lighting */ string ini8;
+        /* Disable Ambient Occlusion */ string ini9;
+        /* Disable Depth of Field */ string ini10;
+        /* Disable Motion Blur */ string ini11;
+        /* Disable Reflections */ string ini12;
+        /* Disable HiRez Water */ string ini13;
+        /* Disable VSync */ string ini14;
+        /* Disable Lens Flare */ string ini15;
+        /* Disable Bokeh */ string ini16;
+        /* Disable Water Reflection */ string ini17;
+        /* Disable Water Depth */ string ini18;
+        /* Disable Gamepad */ string ini19;
+        /* Use better PipBoy menu */ string ini20;
+
+        private void GenerateButton_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+                ini1 = "[ScreenSplatter]\nbBloodSplatterEnabled=0\n";
+          
+            ini2 = ("iSize W=" + textBox1.Text.ToString() + "\n");
+            ini3 = ("iSize H=" + textBox2.Text.ToString() + "\n");
+          
+            if (checkBox2.Checked)
+                ini4 = "[ImageSpace]\nbDoRadialBlur=0\n";
+          
+            if (checkBox3.Checked)
+                ini5 = "bPrecipitation=0\n";
+          
+            if (checkBox4.Checked)
+                ini6 = "bFogEnabled=0\n";
+
+            if (checkBox5.Checked)
+                ini7 = "iMaxAnisotropy=0\n";
+
+            if (checkBox6.Checked)
+                ini8 = "bVolumetricLightingEnable=0\niVolumetricLightingTextureQuality=0\n";
+
+            if (checkBox7.Checked)
+                ini9 = "bSAOEnable=0\n";
+
+            if (checkBox8.Checked)
+                ini10 = "bDoDepthOfField=0\n";
+
+            if (checkBox9.Checked)
+                ini11 = "bMBEnable=0\n";
+
+            if (checkBox10.Checked)
+                ini12 = "[LightingShader]\nbScreenSpaceReflections=0\n";
+
+            if (checkBox11.Checked)
+                ini13 = "bUseWaterHiRes=0\n";
+
+            if (checkBox12.Checked)
+                ini14 = "iPresentInterval=0\n";
+
+            if (checkBox13.Checked)
+                ini15 = "bLensFlare=0\n";
+
+            if (checkBox14.Checked)
+                ini16 = "bScreenSpaceBokeh=0\n";
+
+            if (checkBox15.Checked)
+                ini17 = "bUseWaterReflections=0\n";
+
+            if (checkBox16.Checked)
+                ini18 = "bUseWaterDepth=0\n";
+
+            if (checkBox17.Checked)
+                ini19 = "[General]\nbGamepadEnable=0\n";
+
+            if (checkBox18.Checked)
+                ini20 = "[Pipboy]\nbQuickboyMode=1\n";
+
+            string ini = ini0 + "# Paste this into your Fallout76Custom.ini file\n\n" + ini1 + "[Display]\n" + ini2 + ini3 + ini4 + ini14 + "[Weather]\n" + ini5 + ini6 + "\n# and change these in your Fallout76Prefs.ini file\n\n" + "[Display]\n" + ini7 + ini8 + ini9 + "[ImageSpace]\n" + ini10 + ini12 + ini13 + ini15 + ini16 + "[Water]\n" + ini11 + ini17 + ini18 + ini19 + ini20;
+            Clipboard.SetText(ini);
+            MessageBox.Show(ini);
+        }
+    }
+}
